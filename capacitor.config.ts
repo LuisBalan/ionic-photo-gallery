0ced6b621230e936c312b1a97e4e798ee0ee99e3
LuisBalan <jlbalcz@gmail.com>
2025-04-18 13:38:07 -0600

    last settings to optimize on Android devices

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '042a1261',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
